751377a8cc7ec80bd4b99364c027375ed64767b8
dlflq11@gmail.com <dlflq11@gmail.com>
2024-11-05 21:55:51 +0900

    새싹 풀이

---
+++ src/main/java/baekjun/심화/sprout.java
@@ -0,0 +1,13 @@
+package baekjun.심화;
+
+public class sprout {
+    public static void main(String[] args) {
+        String sprout = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+        System.out.println(sprout);
+    }
+}
